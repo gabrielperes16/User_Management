036cb1aaf11f6e85ff32823e966ec4e864887c39
Gabriel da Costa Peres <105747318+gabrielperes16@users.noreply.github.com>
2024-07-30 20:10:24 -0300

    Delete Sales-Management-main directory

--- Sales-Management-main/Sales Management/database/cliente.py
+++
@@ -1,8 +0,0 @@
-CLIENTES = [
-    {"id": 1, "nome": "Guilherme",'dia': '07/08/2007', 'telefone': '44 99761-4933', 'quantidade': 5},
-    {"id": 2, "nome": "Jorge", 'dia': '07/08/2007', 'telefone': '44 99761-4933', 'quantidade': 0},
-    {"id": 3, "nome": "Maria", 'dia': '07/08/2007', 'telefone': '44 99761-4933', 'quantidade': 0},
-    {"id": 4, "nome": "Felipe",'dia': '07/08/2007', 'telefone': '44 99761-4933', 'quantidade': 0},
-    {"id": 5, "nome": "Joao", 'dia': '07/08/2007', 'telefone': '44 99761-4933', 'quantidade': 0},
-    {"id": 6, "nome": "Jesus", 'dia': '07/08/2007', 'telefone': '44 99761-4933', 'quantidade': 0},
-]
