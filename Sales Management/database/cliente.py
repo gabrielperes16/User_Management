2c06ce74da8751d7f2ee5128029e0796a7fe7c75
Gabriel da Costa Peres <105747318+gabrielperes16@users.noreply.github.com>
2024-07-30 00:24:33 -0300

    Add files via upload

---
+++ Sales Management/database/cliente.py
@@ -0,0 +1,8 @@
+CLIENTES = [
+    {"id":1, "nome": "Guilherme", "email": "teste@teste.com", 'quantidade':0},
+    {"id":2, "nome": "Jorge", "email": "dsadsa@teste.com",'quantidade':0},
+    {"id":3, "nome": "Maria", "email": "zzzzz@teste.com",'quantidade':0},
+    {"id":4, "nome": "Felipe", "email": "dddd@teste.com",'quantidade':0},
+    {"id":5, "nome": "Joao", "email": "ssss@teste.com",'quantidade':0},
+    {"id":6, "nome": "Jesus", "email": "uyyy@teste.com",'quantidade':0},
+]
