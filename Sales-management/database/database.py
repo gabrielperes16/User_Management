edfe0dde1e22be07e075c535a14a33ad887b6caa
gabriel Peres <105747318+gabrielperes16@users.noreply.github.com>
2024-08-28 10:53:49 -0300

    Delete Sales-management directory

--- Sales-management/database/database.py
+++
@@ -1,8 +0,0 @@
-import os
-from peewee import PostgresqlDatabase
-from dotenv import load_dotenv
-
-load_dotenv()
-
-database_uri = os.getenv('DATABASE_URI', '')
-db = PostgresqlDatabase(database_uri)
--- Sales-management/main.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from configuration import configure_all
-
-app = Flask(__name__)
-
-configure_all(app)
-
-app.run(debug=True)
